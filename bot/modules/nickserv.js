e153d14c9458f2ef484c0b472573446068b3325c
Friedemann Altrock <frodenius@gmail.com>
2009-12-04 22:44:13 +0100

    client only

--- bot/modules/nickserv.js
+++
@@ -1,9 +0,0 @@
-/**
- * nickserv options
- *   nick the username to identify
- *   pass the password to use
- */
-
-Bot.addListener("001", function(){
-    Bot.privmsg("NickServ", "identify "+options.nick+" "+options.pass);
-});
