5c75f66f6ad6eae66509ff6467b775c88c9d7768
Friedemann Altrock <frodenius@gmail.com>
2009-12-07 05:33:32 +0100

    changes minimal

--- testbot.js
+++ testbot.js
@@ -1,9 +1,15 @@
-
 var Bot = require('./bot/bot').Bot,
     sys = require('sys');
 
-var bot = new Bot('./config_qnet.json');
+// we do not give a nick, nor user nor realname, the client will 
+// pick a semi-random nick and 'guest' as user/realname.
+var config = {host:"irc.freenode.net", port:6667, nick:""};
 
+var bot = new Bot(config);
+
+function sayHello(from, channel){
+
+// we will get all messages on stdout
 bot.debug = true;
 
 bot.connect();
